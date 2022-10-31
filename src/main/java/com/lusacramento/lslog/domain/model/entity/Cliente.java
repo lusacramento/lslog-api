01ad625ce4ab98b3c91f3582b04cfd29bf03f07d
Lu Sacramento <lucianossacramento@yahoo.com.br>
2022-10-31 20:14:31 -0300

    implementado entidade Cliente

--- src/main/java/com/lusacramento/lslog/domain/model/entity/Cliente.java
+++ src/main/java/com/lusacramento/lslog/domain/model/entity/Cliente.java
@@ -1,2 +1,13 @@
-package com.lusacramento.lslog.domain.model.entity;public class Cliente {
+package com.lusacramento.lslog.domain.model.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Cliente {
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
 }
