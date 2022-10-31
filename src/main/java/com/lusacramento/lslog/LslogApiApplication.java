b4caa315a5ee38d560f6b5126ee0dc236a1796e8
Lu Sacramento <lucianossacramento@yahoo.com.br>
2022-10-31 20:07:03 -0300

    init Spring

--- src/main/java/com/lusacramento/lslog/LslogApiApplication.java
+++ src/main/java/com/lusacramento/lslog/LslogApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LslogApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LslogApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LslogApiApplication.class, args);
+    }
 
 }
