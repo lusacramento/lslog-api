b55b73cd39bfc070d59469ed3d9cc0fd10676ba1
Lu Sacramento <lucianossacramento@yahoo.com.br>
2022-11-03 23:11:47 -0300

    implementado findById para verificacao se existe usuario.

--- src/main/java/com/lusacramento/lslog/domain/service/EntregaService.java
+++ src/main/java/com/lusacramento/lslog/domain/service/EntregaService.java
@@ -2,7 +2,6 @@ package com.lusacramento.lslog.domain.service;
 
 import com.lusacramento.lslog.domain.model.entity.Entrega;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public interface EntregaService {
--- src/main/java/com/lusacramento/lslog/domain/service/Impl/EntregaServiceImpl.java
+++ src/main/java/com/lusacramento/lslog/domain/service/Impl/EntregaServiceImpl.java
@@ -1,13 +1,19 @@
 package com.lusacramento.lslog.domain.service.Impl;
 
+import com.lusacramento.lslog.domain.exception.NegocioException;
+import com.lusacramento.lslog.domain.model.entity.Cliente;
 import com.lusacramento.lslog.domain.model.entity.Entrega;
 import com.lusacramento.lslog.domain.model.entity.StatusEntrega;
 import com.lusacramento.lslog.domain.repository.EntregaRepository;
+import com.lusacramento.lslog.domain.service.ClienteService;
 import com.lusacramento.lslog.domain.service.EntregaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class EntregaServiceImpl implements EntregaService {
@@ -15,9 +21,17 @@ public class EntregaServiceImpl implements EntregaService {
     @Autowired
     private EntregaRepository entregaRepository;
 
+    @Autowired
+    ClienteService clienteService;
+
     @Override
+    @Transactional
     public Entrega save(Entrega entrega) {
+        // Buscando Id
+        Cliente cliente = clienteService.findByIdwithExceptionResponse(entrega.getCliente().getId());
+
         // setando informações pré-definidas pelo sistema.
+        entrega.setCliente(cliente);
         entrega.setStatus(StatusEntrega.PENDENTE);
         entrega.setDataPedido(LocalDateTime.now());
 
