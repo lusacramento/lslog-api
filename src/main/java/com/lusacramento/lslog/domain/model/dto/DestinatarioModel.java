9db7d702560f9fa4f9cd15232a25ed629143c141
Lu Sacramento <lucianossacramento@yahoo.com.br>
2022-11-04 12:25:43 -0300

    Implementado Data Transfer Object Destinatario

---
+++ src/main/java/com/lusacramento/lslog/domain/model/dto/DestinatarioModel.java
@@ -0,0 +1,19 @@
+package com.lusacramento.lslog.domain.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DestinatarioModel {
+
+    private String nome;
+
+    private String logradouro;
+
+    private String numero;
+
+    private String complemento;
+
+    private String bairro;
+}
