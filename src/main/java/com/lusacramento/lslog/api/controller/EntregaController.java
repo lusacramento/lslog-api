b968492a013b6261ddf85d99b5ddce03bfb021b4
Lu Sacramento <lucianossacramento@yahoo.com.br>
2022-11-04 00:19:48 -0300

    implementado findAll e findById no controller da Entrega

--- src/main/java/com/lusacramento/lslog/api/controller/EntregaController.java
+++ src/main/java/com/lusacramento/lslog/api/controller/EntregaController.java
@@ -4,7 +4,11 @@ import com.lusacramento.lslog.domain.model.entity.Entrega;
 import com.lusacramento.lslog.domain.service.EntregaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/entregas")
@@ -16,9 +20,23 @@ public class EntregaController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Entrega save(@RequestBody Entrega entrega){
+    public Entrega save(@Valid @RequestBody Entrega entrega){
         return entregaService.save(entrega);
     }
+
+    @GetMapping
+    public List<Entrega> findAll(){
+        return entregaService.findAll();
+    }
+
+    @GetMapping("/{entregaId}")
+    public ResponseEntity<Entrega> findById(@PathVariable Long entregaId){
+        return entregaService.findById(entregaId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+
 
 
 }
