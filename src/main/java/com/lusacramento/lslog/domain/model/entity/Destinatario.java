52d842ee0b54505f5717d358377740223d31174d
Lu Sacramento <lucianossacramento@yahoo.com.br>
2022-11-04 00:04:35 -0300

    adicionando validations nos campos.

--- src/main/java/com/lusacramento/lslog/domain/model/entity/Destinatario.java
+++ src/main/java/com/lusacramento/lslog/domain/model/entity/Destinatario.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
@@ -12,18 +13,23 @@ import javax.persistence.*;
 public class Destinatario {
 
     // Não tem id porque esta entidade vai estar embutida dentro de uma tabela do bd.
+    @NotBlank
     @Column(name = "destinatario_nome")
     private String nome;
 
+    @NotBlank
     @Column(name = "destinatario_logradouro")
     private String logradouro;
 
+    @NotBlank
     @Column(name = "destinatario_numero")
     private String numero;
 
+    @NotBlank
     @Column(name="destinatario_complemento")
     private String complemento;
 
+    @NotBlank
     @Column(name = "destinatario_bairro")
     private String bairro;
 }
--- src/main/java/com/lusacramento/lslog/domain/model/entity/Entrega.java
+++ src/main/java/com/lusacramento/lslog/domain/model/entity/Entrega.java
@@ -1,11 +1,16 @@
 package com.lusacramento.lslog.domain.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.lusacramento.lslog.domain.ValidationGroups;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.groups.ConvertGroup;
+import javax.validation.groups.Default;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -21,12 +26,18 @@ public class Entrega {
     private Long id;
 
 //    @JoinColumn(name = "cliente_id")  Caso a coluna do bd for outro nome, usar esta anotação
+    @NotNull
     @ManyToOne // Muitas entregas possuem 1 cliente
+    @Valid
+    @ConvertGroup(from = Default.class, to = ValidationGroups.ClienteId.class)
     private Cliente cliente;
 
+    @Valid
+    @NotNull
     @Embedded // os dados desta entidade está contido na nesta tabela do banco de dados.
     private Destinatario destinatario;
 
+    @NotNull
     private BigDecimal taxa;
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
